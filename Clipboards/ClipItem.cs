37816c709e5c5c081566c70dbd95e94ee92f3e34
Jean-Michel Frouin <jmfrouin@gmail.com>
2011-09-26 22:17:28 +0200

    Initial import

---
+++ Clipboards/ClipItem.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Clipboards
+{
+    [Serializable()]
+    public class ClipItem
+    {
+        #region Enumeration, types, accessors & mutators.        
+        public enum EType
+        {
+            eAudio,
+            eCustom,
+            eDrop,
+            eHTML,
+            eImage,
+            eNone,
+            eRaw,
+            eRTF,
+            eText
+        }
+
+        private EType fType = EType.eNone;
+        public EType Type
+        {
+            get { return fType; }
+            set { fType = value; }
+        }
+
+        private string fContent = string.Empty;
+        public string Content
+        {
+            get { return fContent; }
+            set { fContent = value; }
+        }
+
+        private Rectangle fImagePreview;
+        private Image fImage = null;
+        public Image Image
+        {
+            get { return fImage; }
+            set 
+            { 
+                fImage = value;
+                int H = fImage.Height;
+                int W = fImage.Width;
+                if( H > 100 || W > 100)
+                {
+                    W = H < W ? 100 : ((W * 100) / H);
+                    H = H < W ? ((H * 100) / W) : 100;
+                }
+                fImagePreview = new Rectangle(0, 0, W, H);
+            }
+        }
+        
+        private Image fOrigProgSmallIcon = null;
+        private Image fOrigProgLargeIcon = null;
+
+        private string fExePath = null;
+        public string ExePath
+        {
+            get { return fExePath; }
+            set { fExePath = value; }
+        }
+
+        public DateTime fTime = DateTime.Now;
+        #endregion
+        
+        #region Constructors
+        public ClipItem(string ep)
+        {
+            fExePath = ep;
+            fOrigProgSmallIcon = ShellIcon.GetSmallIcon(ep).ToBitmap();
+            fOrigProgLargeIcon = ShellIcon.GetLargeIcon(ep).ToBitmap();
+        }
+        #endregion
+
+        public void Measure(MeasureItemEventArgs e, Font font, ref int w, ref int h)
+        {
+            Rectangle imageRect;
+            //Handle thumbnail
+            if (fImage != null)
+            {
+                imageRect = fImagePreview;
+                //w = imageRect.Width;
+                h = imageRect.Height + 4;
+            }
+            
+            if (fContent != string.Empty)
+            {
+                SizeF size = e.Graphics.MeasureString(fContent, font);
+                h = (int)size.Height + 4;
+            }
+            
+            //Minimal size for Icon
+            if (h < 16 + 4)
+            {
+                h = 16 + 4;
+            }
+        }
+
+        public void Draw(Graphics g, Rectangle bounds, DrawItemState state, Font font)
+        {
+            //Draw border if item is selected only! 
+            Rectangle borderRect = bounds;
+            borderRect.Width = bounds.Width - 1;
+                
+            System.Drawing.SolidBrush myBrush;
+            if ((state & DrawItemState.Selected) == DrawItemState.Selected)
+            {
+                Color myColor = Color.FromArgb(0xFF, 0xCC, 0xCC, 0xFF);
+                myBrush = new System.Drawing.SolidBrush(myColor);
+            }
+            else
+            {
+                myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+            }
+            g.FillRectangle(myBrush, borderRect);
+            g.DrawRectangle(Pens.Black, borderRect);
+            
+            //Draw text I/A
+            if (fContent != string.Empty)
+            {
+                Brush textBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+                Rectangle textRect = bounds;
+                textRect.X = bounds.X + 4 + (bounds.Height < 36 ? 16 : 32);
+                textRect.Y = bounds.Y + 2;
+                g.DrawString(fContent, font, textBrush, textRect);
+            }
+
+            //Draw thumbnail I/A
+            if (fImage != null)
+            {
+                Rectangle imageRect = fImagePreview;
+                imageRect.X = bounds.Width - fImagePreview.Width - 3;
+                imageRect.Y = bounds.Y + 2;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(fImage, imageRect);
+                g.DrawRectangle(Pens.Black, imageRect);
+            }
+
+            //Draw the application icon  ! 
+            if (fOrigProgLargeIcon != null && bounds.Height > 36)
+            {
+                int X = bounds.Width - 2 - fOrigProgLargeIcon.Width;
+                g.DrawImage(fOrigProgLargeIcon, bounds.X + 2, bounds.Y + 2);
+            }
+            else 
+            {
+                if (fOrigProgSmallIcon != null)
+                {
+                    int X = bounds.Width - 2 - fOrigProgSmallIcon.Width;
+                    g.DrawImage(fOrigProgSmallIcon, bounds.X + 2, bounds.Y + 2);
+                }
+            }
+        }
+    }
+}
---
+++ Clipboards/MainForm.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Clipboards
+{
+    public partial class MainForm : Form
+    {
+        #region Members data
+        //Used when registering/unregistering the clipboard viewer!
+        private IntPtr ClipboardViewerNext;
+
+        //List of Clips
+        List<ClipItem> fClips = new List<ClipItem>();
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //Callbacks
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Closed += new System.EventHandler(this.MainForm_Closed);
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
+            this.Deactivate += new System.EventHandler(this.MainForm_Deactivate);
+
+            //Settings
+            Screen screen = Screen.PrimaryScreen;
+            int ScreenW = screen.Bounds.Width;
+            int ScreenH = screen.Bounds.Height;
+            int AppW = this.Size.Width;
+            int AppH = this.Size.Height;
+            int TaskbarH = Screen.PrimaryScreen.Bounds.Bottom - Screen.PrimaryScreen.WorkingArea.Bottom;
+            this.Location = new Point(ScreenW - AppW, ScreenH - AppH - TaskbarH);
+
+            //Folders
+            if (System.IO.Directory.Exists(@"Clips"))
+            {
+                System.IO.Directory.Delete(@"Clips", true);
+            }
+            System.IO.Directory.CreateDirectory(@"Clips");
+
+            RegisterClipboardViewer();
+        }
+
+        #region Callbacks !
+        private void MainForm_Activated(object sender, System.EventArgs e)
+        {
+            this.Opacity = 1;
+        }
+
+        private void MainForm_Deactivate(object sender, System.EventArgs e)
+        {
+            this.Opacity = 0.3;
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            //CTRL+D
+            APIFuncs.RegisterHotKey(this.Handle, this.GetType().GetHashCode(), 2, (int)'D');
+            RestoreClips();
+        }
+
+        private void MainForm_Closed(object sender, System.EventArgs e)
+        {
+            APIFuncs.UnregisterHotKey(this.Handle, this.GetType().GetHashCode());
+            UnregisterClipboardViewer();
+            SaveClips();
+        }
+        #endregion
+
+        private void InsertClip()
+        {
+            IDataObject iData = Clipboard.GetDataObject();
+            if (iData != null)
+            {
+                //Handle TXT clipboard
+                if (iData.GetDataPresent(DataFormats.Text))
+                {
+                    IntPtr hwnd = APIFuncs.getforegroundWindow();
+                    Int32 pid = APIFuncs.GetWindowProcessID(hwnd);
+                    Process p = Process.GetProcessById(pid);
+
+                    ClipItem Item = new ClipItem(p.MainModule.FileName);
+                    Item.Content = iData.GetData(DataFormats.StringFormat).ToString();
+                    fClips.Add(Item);
+
+                    listBoxClips.Items.Add(fClips.Count.ToString());
+                }
+
+                //Handle Bitmap element
+                if (iData.GetDataPresent(DataFormats.Bitmap))
+                {
+                    IntPtr hwnd = APIFuncs.getforegroundWindow();
+                    Int32 pid = APIFuncs.GetWindowProcessID(hwnd);
+                    Process p = Process.GetProcessById(pid);
+
+                    ClipItem Item = new ClipItem(p.MainModule.FileName);
+                    Item.Image = (Bitmap)iData.GetData(DataFormats.Bitmap);
+                    fClips.Add(Item);
+
+                    listBoxClips.Items.Add(fClips.Count.ToString());
+                }
+            }
+        }
+
+        #region Customized listbox ! 
+        private void MeasureItem(object sender, MeasureItemEventArgs e)
+        {
+            ClipItem Clip = fClips[e.Index];
+            int W = e.ItemWidth, H = e.ItemHeight;
+            Clip.Measure(e, listBoxClips.Font, ref W, ref H);
+            e.ItemWidth = W;
+            e.ItemHeight = H;
+        }
+
+        private void DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (e.Index != -1)
+            {
+                ClipItem Clip = fClips[e.Index];
+                Clip.Draw(g, e.Bounds, e.State, e.Font);
+            }
+            g.Dispose();
+        }
+        #endregion
+
+        #region (Un)Register to clipboard viewers chain !
+        private void RegisterClipboardViewer()
+        {
+            ClipboardViewerNext = APIFuncs.SetClipboardViewer(this.Handle);
+        }
+
+        private void UnregisterClipboardViewer()
+        {
+            APIFuncs.ChangeClipboardChain(this.Handle, ClipboardViewerNext);
+        }
+        #endregion
+
+        protected override void WndProc(ref Message m)
+        {
+            switch ((Clipboards.APIFuncs.Msgs)m.Msg)
+            {
+                case Clipboards.APIFuncs.Msgs.WM_HOTKEY:
+                {
+                    InsertClip();
+                    this.Activate();
+                    break;
+                }
+                case Clipboards.APIFuncs.Msgs.WM_DRAWCLIPBOARD:
+                {
+                    InsertClip();
+                    APIFuncs.SendMessage(ClipboardViewerNext, m.Msg, m.WParam, m.LParam);
+                    break;
+                }
+                case Clipboards.APIFuncs.Msgs.WM_CHANGECBCHAIN:
+                {
+                    if (m.WParam == ClipboardViewerNext)
+                    {
+                        ClipboardViewerNext = m.LParam;
+                    }
+                    else
+                    {
+                        APIFuncs.SendMessage(ClipboardViewerNext, m.Msg, m.WParam, m.LParam);
+                    }
+                    break;
+                }
+                default:
+                {
+                    base.WndProc(ref m);
+                    break;
+                }
+            }
+        }
+
+        #region toolStrip callbacks
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox aBox = new AboutBox();
+            aBox.Show();
+        }
+
+        private void toolStripDelete_Click(object sender, EventArgs e)
+        {
+            int Index = listBoxClips.SelectedIndex;
+            if (Index != -1)
+            {
+                fClips.RemoveAt(Index);
+                listBoxClips.Items.RemoveAt(Index);
+            }
+        }
+        #endregion
+
+        #region Save / Restore Clips
+        private void SaveClips()
+        {
+            try
+            {
+                using (Stream stream = File.Open("Clips.bin", FileMode.Create))
+                {
+                    BinaryFormatter bin = new BinaryFormatter();
+                    bin.Serialize(stream, fClips);
+                }
+            }
+            catch (IOException)
+            {
+            }
+        }
+
+        private void RestoreClips()
+        {
+            try
+            {
+                using (Stream stream = File.Open("Clips.bin", FileMode.Open))
+                {
+                    BinaryFormatter bin = new BinaryFormatter();
+                    fClips = (List<ClipItem>)bin.Deserialize(stream);
+                    foreach (ClipItem Item in fClips)
+                    {
+                        listBoxClips.Items.Add("Clip!");
+                    }
+                }
+            }
+            catch (IOException)
+            {
+            }
+        }
+        #endregion
+        
+    }
+}
