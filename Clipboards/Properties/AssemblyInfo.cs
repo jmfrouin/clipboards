eccec79e83818f6cfa3b0c9bd702c54ea58cbc83
Jean-Michel Frouin <jmfrouin@gmail.com>
2011-10-18 20:49:34 +0200

    Version 0.12

--- Clipboards/Properties/AssemblyInfo.cs
+++ Clipboards/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Clipboards")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A clipboards manager written in .Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Anneis")]
 [assembly: AssemblyProduct("Clipboards")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.*")]
+[assembly: AssemblyVersion("0.12.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
