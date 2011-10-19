e47583aca4132f2b78f87272ff5ba7a61bc2cfe4
Jean-Michel Frouin <jmfrouin@gmail.com>
2011-10-19 19:55:21 +0200

    Ajout d'un script pour nettoyer, les sources, avant archivage.

---
+++ Clean.bat
@@ -0,0 +1,11 @@
+rem Nettoyage des fichiers inutiles, avant archivage
+echo off 
+cls
+del /S /Q Clipboards\bin
+del /S /Q Clipboards\obj
+del /S /Q Setup\Debug
+del /S /Q Setup64\Debug
+del /S /Q Setup\Release
+del /S /Q Setup64\Release
+
+pause
